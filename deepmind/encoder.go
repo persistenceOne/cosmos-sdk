patiee/tendermint

Add Cosmos firehose extractor

deepmind/encoder.go
@@ -1,0 +1,117 @@
+package deepmind
+
+import (
+	"fmt"
+
+	pbcosmos "github.com/figment-networks/proto-cosmos/pb/sf/cosmos/type/v1"
+	"github.com/golang/protobuf/proto"
+	abci "github.com/tendermint/tendermint/abci/types"
+	"github.com/tendermint/tendermint/crypto/tmhash"
+	"github.com/tendermint/tendermint/types"
+)
+
+func encodeBlock(bh types.EventDataNewBlock) ([]byte, error) {
+	mappedEvidence, err := mapEvidence(&bh.Block.Evidence)
+	if err != nil {
+		return nil, err
+	}
+
+	mappedCommitSignatures, err := mapSignatures(bh.Block.LastCommit.Signatures)
+	if err != nil {
+		return nil, err
+	}
+
+	mappedResponseEndBlock, err := mapResponseEndBlock(&bh.ResultEndBlock)
+	if err != nil {
+		return nil, err
+	}
+
+	nb := &pbcosmos.Block{
+		Header: &pbcosmos.Header{
+			Version: &pbcosmos.Consensus{
+				Block: bh.Block.Header.Version.Block,
+				App:   bh.Block.Header.Version.App,
+			},
+			ChainId:            bh.Block.Header.ChainID,
+			Height:             uint64(bh.Block.Header.Height),
+			Time:               mapTimestamp(bh.Block.Header.Time),
+			LastBlockId:        mapBlockID(bh.Block.LastBlockID),
+			LastCommitHash:     bh.Block.Header.LastCommitHash,
+			DataHash:           bh.Block.Header.DataHash,
+			ValidatorsHash:     bh.Block.Header.ValidatorsHash,
+			NextValidatorsHash: bh.Block.Header.NextValidatorsHash,
+			ConsensusHash:      bh.Block.Header.ConsensusHash,
+			AppHash:            bh.Block.Header.AppHash,
+			LastResultsHash:    bh.Block.Header.LastResultsHash,
+			EvidenceHash:       bh.Block.Header.EvidenceHash,
+			ProposerAddress:    bh.Block.Header.ProposerAddress,
+			Hash:               bh.Block.Header.Hash(),
+		},
+		Evidence: mappedEvidence,
+		LastCommit: &pbcosmos.Commit{
+			Height:     bh.Block.LastCommit.Height,
+			Round:      bh.Block.LastCommit.Round,
+			BlockId:    mapBlockID(bh.Block.LastCommit.BlockID),
+			Signatures: mappedCommitSignatures,
+		},
+		ResultBeginBlock: mapResponseBeginBlock(&bh.ResultBeginBlock),
+		ResultEndBlock:   mappedResponseEndBlock,
+	}
+
+	return proto.Marshal(nb)
+}
+
+func encodeTx(result *abci.TxResult) ([]byte, error) {
+	mappedTx, err := mapTx(result.Tx)
+	if err != nil {
+		return nil, err
+	}
+
+	tx := &pbcosmos.TxResult{
+		Hash:   tmhash.Sum(result.Tx),
+		Height: uint64(result.Height),
+		Index:  result.Index,
+		Tx:     mappedTx,
+		Result: &pbcosmos.ResponseDeliverTx{
+			Code:      result.Result.Code,
+			Data:      result.Result.Data,
+			Log:       result.Result.Log,
+			Info:      result.Result.Info,
+			GasWanted: result.Result.GasWanted,
+			GasUsed:   result.Result.GasUsed,
+			Codespace: result.Result.Codespace,
+		},
+	}
+
+	for _, ev := range result.Result.Events {
+		tx.Result.Events = append(tx.Result.Events, mapEvent(ev))
+	}
+
+	return proto.Marshal(tx)
+}
+
+func encodeValidatorSetUpdates(updates *types.EventDataValidatorSetUpdates) ([]byte, error) {
+	result := &pbcosmos.ValidatorSetUpdates{}
+
+	for _, update := range updates.ValidatorUpdates {
+		nPK := &pbcosmos.PublicKey{}
+
+		switch update.PubKey.Type() {
+		case "ed25519":
+			nPK.Sum = &pbcosmos.PublicKey_Ed25519{Ed25519: update.PubKey.Bytes()}
+		case "secp256k1":
+			nPK.Sum = &pbcosmos.PublicKey_Secp256K1{Secp256K1: update.PubKey.Bytes()}
+		default:
+			return nil, fmt.Errorf("unsupported pubkey type: %T", update.PubKey)
+		}
+
+		result.ValidatorUpdates = append(result.ValidatorUpdates, &pbcosmos.Validator{
+			Address:          update.Address.Bytes(),
+			VotingPower:      update.VotingPower,
+			ProposerPriority: update.ProposerPriority,
+			PubKey:           nPK,
+		})
+	}
+
+	return proto.Marshal(result)
+}

deepmind/extractor.go
@@ -1,0 +1,142 @@
+package deepmind
+
+import (
+	"context"
+	"encoding/base64"
+	"time"
+
+	"github.com/figment-networks/extractor-cosmos"
+	"github.com/tendermint/tendermint/types"
+)
+
+var (
+	enabled            bool
+	currentBlockHeight int64
+	inBlock            bool
+)
+
+func Enable() {
+	enabled = true
+}
+
+func IsEnabled() bool {
+	return enabled
+}
+
+func Disable() {
+	enabled = false
+}
+
+func Initialize(config *extractor.Config) {
+	extractor.SetWriterFromConfig(config)
+	enabled = true
+}
+
+func Shutdown(ctx context.Context) {
+	defer func() {
+		enabled = false
+	}()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-time.Tick(time.Second):
+			if !inBlock {
+				return
+			}
+		}
+	}
+}
+
+func Abort(err error) {
+	panic(err)
+}
+
+func BeginBlock(height int64) error {
+	if !enabled {
+		return nil
+	}
+
+	if height == currentBlockHeight {
+		panic("cannot initialize the same height more than once")
+	}
+
+	currentBlockHeight = height
+	inBlock = true
+
+	err := extractor.SetHeight(height)
+	if err != nil {
+		return err
+	}
+	return extractor.WriteLine(extractor.MsgBegin, "%d", height)
+}
+
+func FinalizeBlock(height int64) error {
+	if !enabled {
+		return nil
+	}
+
+	if height != currentBlockHeight {
+		panic("finalize block on invalid height")
+	}
+
+	inBlock = false
+
+	return extractor.WriteLine(extractor.MsgEnd, "%d", height)
+}
+
+func AddBlockData(data types.EventDataNewBlock) error {
+	if !enabled {
+		return nil
+	}
+
+	buff, err := encodeBlock(data)
+	if err != nil {
+		return err
+	}
+
+	return extractor.WriteLine(extractor.MsgBlock, "%s", base64.StdEncoding.EncodeToString(buff))
+}
+
+func AddBlockHeaderData(data types.EventDataNewBlockHeader) error {
+	if !enabled {
+		return nil
+	}
+	// Skipped for now
+	return nil
+}
+
+func AddEvidenceData(data types.EventDataNewEvidence) error {
+	if !enabled {
+		return nil
+	}
+	// Skipped for now
+	return nil
+}
+
+func AddTransactionData(tx types.EventDataTx) error {
+	if !enabled {
+		return nil
+	}
+
+	data, err := encodeTx(&tx.TxResult)
+	if err != nil {
+		return err
+	}
+
+	return extractor.WriteLine(extractor.MsgTx, "%s", base64.StdEncoding.EncodeToString(data))
+}
+
+func AddValidatorSetUpdatesData(updates types.EventDataValidatorSetUpdates) error {
+	if !enabled {
+		return nil
+	}
+
+	data, err := encodeValidatorSetUpdates(&updates)
+	if err != nil {
+		return err
+	}
+
+	return extractor.WriteLine(extractor.MsgValidatorSetUpdate, "%s", base64.StdEncoding.EncodeToString(data))
+}

node/node.go
@@ -56,6 +56,8 @@ import (
 	_ "net/http/pprof" //nolint: gosec // securely exposed on separate, optional port
 
 	_ "github.com/lib/pq" // provide the psql db driver
+
+	"github.com/tendermint/tendermint/deepmind"
 )
 
 //------------------------------------------------------------------------------
@@ -748,6 +750,12 @@ func NewNode(config *cfg.Config,
 		return nil, err
 	}
 
+	// Initialize data extraction
+	if config.Extractor.Enabled {
+		deepmind.Initialize(config.Extractor)
+		logger.Info("Initialized extractor module", "output", config.Extractor.OutputFile)
+	}
+
 	// If an address is provided, listen on the socket for a connection from an
 	// external signing process.
 	if config.PrivValidatorListenAddr != "" {
@@ -1057,6 +1065,10 @@ func (n *Node) OnStop() {
 		if err := n.stateStore.Close(); err != nil {
 			n.Logger.Error("problem closing statestore", "err", err)
 		}
+	}
+	if deepmind.IsEnabled() {
+		n.Logger.Info("waiting for last block finalization", "module", "deepmind")
+		deepmind.Shutdown(context.Background())
 	}
 }
 

statesync/reactor.go
@@ -2,6 +2,7 @@ package statesync
 
 import (
 	"errors"
+	"os"
 	"sort"
 	"time"
 
@@ -25,6 +26,8 @@ const (
 	// recentSnapshots is the number of recent snapshots to send and receive per peer.
 	recentSnapshots = 10
 )
+
+var requestSnapshots = true
 
 // Reactor handles state sync, both restoring snapshots for the local node and serving snapshots
 // for other nodes.
@@ -280,6 +283,12 @@ func (r *Reactor) recentSnapshots(n uint32) ([]*snapshot, error) {
 // Sync runs a state sync, returning the new state and last commit at the snapshot height.
 // The caller must store the state and commit in the state database and block store.
 func (r *Reactor) Sync(stateProvider StateProvider, discoveryTime time.Duration) (sm.State, *types.Commit, error) {
+	var (
+		state  sm.State
+		commit *types.Commit
+		err    error
+	)
+
 	r.mtx.Lock()
 	if r.syncer != nil {
 		r.mtx.Unlock()
@@ -288,19 +297,31 @@ func (r *Reactor) Sync(stateProvider StateProvider, discoveryTime time.Duration)
 	r.syncer = newSyncer(r.cfg, r.Logger, r.conn, r.connQuery, stateProvider, r.tempDir)
 	r.mtx.Unlock()
 
-	hook := func() {
-		r.Logger.Debug("Requesting snapshots from known peers")
-		// Request snapshots from all currently connected peers
+	if snapshotDir := os.Getenv("SNAPSHOT_IMPORT_DIR"); snapshotDir != "" {
+		// Do not request snapshots or chunks from the network since we're restoring from a static snapshot
+		requestSnapshots = false
 
-		r.Switch.BroadcastEnvelope(p2p.Envelope{
-			ChannelID: SnapshotChannel,
-			Message:   &ssproto.SnapshotsRequest{},
-		})
+		state, commit, err = r.syncer.SyncFromLocalSnapshot(snapshotDir)
+		if err != nil {
+			// Blow up here so it's easier to spot any issues with snapshot restore
+			panic(err)
+		}
+	} else {
+
+		hook := func() {
+			r.Logger.Debug("Requesting snapshots from known peers")
+			// Request snapshots from all currently connected peers
+
+			r.Switch.BroadcastEnvelope(p2p.Envelope{
+				ChannelID: SnapshotChannel,
+				Message:   &ssproto.SnapshotsRequest{},
+			})
+		}
+
+		hook()
+
+		state, commit, err = r.syncer.SyncAny(discoveryTime, hook)
 	}
-
-	hook()
-
-	state, commit, err := r.syncer.SyncAny(discoveryTime, hook)
 
 	r.mtx.Lock()
 	r.syncer = nil

persistenceOne/cosmos-sdk

deepmind/encoder.go
@@ -1,0 +1,117 @@
+package deepmind
+
+import (
+	"fmt"
+
+	abci "github.com/cometbft/cometbft/abci/types"
+	"github.com/cometbft/cometbft/crypto/tmhash"
+	"github.com/cometbft/cometbft/types"
+	pbcosmos "github.com/figment-networks/proto-cosmos/pb/sf/cosmos/type/v1"
+	"github.com/golang/protobuf/proto"
+)
+
+func encodeBlock(bh types.EventDataNewBlock) ([]byte, error) {
+	mappedEvidence, err := mapEvidence(&bh.Block.Evidence)
+	if err != nil {
+		return nil, err
+	}
+
+	mappedCommitSignatures, err := mapSignatures(bh.Block.LastCommit.Signatures)
+	if err != nil {
+		return nil, err
+	}
+
+	mappedResponseEndBlock, err := mapResponseEndBlock(&bh.ResultEndBlock)
+	if err != nil {
+		return nil, err
+	}
+
+	nb := &pbcosmos.Block{
+		Header: &pbcosmos.Header{
+			Version: &pbcosmos.Consensus{
+				Block: bh.Block.Header.Version.Block,
+				App:   bh.Block.Header.Version.App,
+			},
+			ChainId:            bh.Block.Header.ChainID,
+			Height:             uint64(bh.Block.Header.Height),
+			Time:               mapTimestamp(bh.Block.Header.Time),
+			LastBlockId:        mapBlockID(bh.Block.LastBlockID),
+			LastCommitHash:     bh.Block.Header.LastCommitHash,
+			DataHash:           bh.Block.Header.DataHash,
+			ValidatorsHash:     bh.Block.Header.ValidatorsHash,
+			NextValidatorsHash: bh.Block.Header.NextValidatorsHash,
+			ConsensusHash:      bh.Block.Header.ConsensusHash,
+			AppHash:            bh.Block.Header.AppHash,
+			LastResultsHash:    bh.Block.Header.LastResultsHash,
+			EvidenceHash:       bh.Block.Header.EvidenceHash,
+			ProposerAddress:    bh.Block.Header.ProposerAddress,
+			Hash:               bh.Block.Header.Hash(),
+		},
+		Evidence: mappedEvidence,
+		LastCommit: &pbcosmos.Commit{
+			Height:     bh.Block.LastCommit.Height,
+			Round:      bh.Block.LastCommit.Round,
+			BlockId:    mapBlockID(bh.Block.LastCommit.BlockID),
+			Signatures: mappedCommitSignatures,
+		},
+		ResultBeginBlock: mapResponseBeginBlock(&bh.ResultBeginBlock),
+		ResultEndBlock:   mappedResponseEndBlock,
+	}
+
+	return proto.Marshal(nb)
+}
+
+func encodeTx(result *abci.TxResult) ([]byte, error) {
+	mappedTx, err := mapTx(result.Tx)
+	if err != nil {
+		return nil, err
+	}
+
+	tx := &pbcosmos.TxResult{
+		Hash:   tmhash.Sum(result.Tx),
+		Height: uint64(result.Height),
+		Index:  result.Index,
+		Tx:     mappedTx,
+		Result: &pbcosmos.ResponseDeliverTx{
+			Code:      result.Result.Code,
+			Data:      result.Result.Data,
+			Log:       result.Result.Log,
+			Info:      result.Result.Info,
+			GasWanted: result.Result.GasWanted,
+			GasUsed:   result.Result.GasUsed,
+			Codespace: result.Result.Codespace,
+		},
+	}
+
+	for _, ev := range result.Result.Events {
+		tx.Result.Events = append(tx.Result.Events, mapEvent(ev))
+	}
+
+	return proto.Marshal(tx)
+}
+
+func encodeValidatorSetUpdates(updates *types.EventDataValidatorSetUpdates) ([]byte, error) {
+	result := &pbcosmos.ValidatorSetUpdates{}
+
+	for _, update := range updates.ValidatorUpdates {
+		nPK := &pbcosmos.PublicKey{}
+
+		switch update.PubKey.Type() {
+		case "ed25519":
+			nPK.Sum = &pbcosmos.PublicKey_Ed25519{Ed25519: update.PubKey.Bytes()}
+		case "secp256k1":
+			nPK.Sum = &pbcosmos.PublicKey_Secp256K1{Secp256K1: update.PubKey.Bytes()}
+		default:
+			return nil, fmt.Errorf("unsupported pubkey type: %T", update.PubKey)
+		}
+
+		result.ValidatorUpdates = append(result.ValidatorUpdates, &pbcosmos.Validator{
+			Address:          update.Address.Bytes(),
+			VotingPower:      update.VotingPower,
+			ProposerPriority: update.ProposerPriority,
+			PubKey:           nPK,
+		})
+	}
+
+	return proto.Marshal(result)
+}

deepmind/extractor.go
@@ -1,0 +1,142 @@
+package deepmind
+
+import (
+	"context"
+	"encoding/base64"
+	"time"
+
+	"github.com/cometbft/cometbft/types"
+	"github.com/figment-networks/extractor-cosmos"
+)
+
+var (
+	enabled            bool
+	currentBlockHeight int64
+	inBlock            bool
+)
+
+func Enable() {
+	enabled = true
+}
+
+func IsEnabled() bool {
+	return enabled
+}
+
+func Disable() {
+	enabled = false
+}
+
+func Initialize(config *extractor.Config) {
+	extractor.SetWriterFromConfig(config)
+	enabled = true
+}
+
+func Shutdown(ctx context.Context) {
+	defer func() {
+		enabled = false
+	}()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-time.Tick(time.Second):
+			if !inBlock {
+				return
+			}
+		}
+	}
+}
+
+func Abort(err error) {
+	panic(err)
+}
+
+func BeginBlock(height int64) error {
+	if !enabled {
+		return nil
+	}
+
+	if height == currentBlockHeight {
+		panic("cannot initialize the same height more than once")
+	}
+
+	currentBlockHeight = height
+	inBlock = true
+
+	err := extractor.SetHeight(height)
+	if err != nil {
+		return err
+	}
+	return extractor.WriteLine(extractor.MsgBegin, "%d", height)
+}
+
+func FinalizeBlock(height int64) error {
+	if !enabled {
+		return nil
+	}
+
+	if height != currentBlockHeight {
+		panic("finalize block on invalid height")
+	}
+
+	inBlock = false
+
+	return extractor.WriteLine(extractor.MsgEnd, "%d", height)
+}
+
+func AddBlockData(data types.EventDataNewBlock) error {
+	if !enabled {
+		return nil
+	}
+
+	buff, err := encodeBlock(data)
+	if err != nil {
+		return err
+	}
+
+	return extractor.WriteLine(extractor.MsgBlock, "%s", base64.StdEncoding.EncodeToString(buff))
+}
+
+func AddBlockHeaderData(data types.EventDataNewBlockHeader) error {
+	if !enabled {
+		return nil
+	}
+	// Skipped for now
+	return nil
+}
+
+func AddEvidenceData(data types.EventDataNewEvidence) error {
+	if !enabled {
+		return nil
+	}
+	// Skipped for now
+	return nil
+}
+
+func AddTransactionData(tx types.EventDataTx) error {
+	if !enabled {
+		return nil
+	}
+
+	data, err := encodeTx(&tx.TxResult)
+	if err != nil {
+		return err
+	}
+
+	return extractor.WriteLine(extractor.MsgTx, "%s", base64.StdEncoding.EncodeToString(data))
+}
+
+func AddValidatorSetUpdatesData(updates types.EventDataValidatorSetUpdates) error {
+	if !enabled {
+		return nil
+	}
+
+	data, err := encodeValidatorSetUpdates(&updates)
+	if err != nil {
+		return err
+	}
+
+	return extractor.WriteLine(extractor.MsgValidatorSetUpdate, "%s", base64.StdEncoding.EncodeToString(data))
+}

node/node.go
@@ -40,6 +40,11 @@ import (
 	"github.com/cometbft/cometbft/version"
 
 	_ "net/http/pprof" //nolint: gosec
+	_ "net/http/pprof" //nolint: gosec // securely exposed on separate, optional port
+
+	_ "github.com/lib/pq" // provide the psql db driver
+
+	"github.com/cometbft/cometbft/deepmind"
 )
 
 // Node is the highest level interface to a full CometBFT node.
@@ -328,6 +333,12 @@ func NewNodeWithContext(ctx context.Context,
 		genDoc.ChainID, dbProvider, eventBus, logger)
 	if err != nil {
 		return nil, err
+	}
+
+	// Initialize data extraction
+	if config.Extractor.Enabled {
+		deepmind.Initialize(config.Extractor)
+		logger.Info("Initialized extractor module", "output", config.Extractor.OutputFile)
 	}
 
 	// If an address is provided, listen on the socket for a connection from an
@@ -650,6 +661,10 @@ func (n *Node) OnStop() {
 		if err := n.EvidencePool().Close(); err != nil {
 			n.Logger.Error("problem closing evidencestore", "err", err)
 		}
+	if deepmind.IsEnabled() {
+		n.Logger.Info("waiting for last block finalization", "module", "deepmind")
+		deepmind.Shutdown(context.Background())
+	}
 	}
 }
 

statesync/reactor.go
@@ -3,6 +3,7 @@ package statesync
 import (
 	"context"
 	"errors"
+	"os"
 	"sort"
 	"time"
 
@@ -24,6 +25,8 @@ const (
 	// recentSnapshots is the number of recent snapshots to send and receive per peer.
 	recentSnapshots = 10
 )
+
+var requestSnapshots = true
 
 // Reactor handles state sync, both restoring snapshots for the local node and serving snapshots
 // for other nodes.
@@ -95,7 +98,7 @@ func (r *Reactor) AddPeer(peer p2p.Peer) {
 }
 
 // RemovePeer implements p2p.Reactor.
-func (r *Reactor) RemovePeer(peer p2p.Peer, _ interface{}) {
+func (r *Reactor) RemovePeer(peer p2p.Peer, _ any) {
 	r.mtx.RLock()
 	defer r.mtx.RUnlock()
 	if r.syncer != nil {
@@ -262,6 +265,12 @@ func (r *Reactor) recentSnapshots(n uint32) ([]*snapshot, error) {
 // Sync runs a state sync, returning the new state and last commit at the snapshot height.
 // The caller must store the state and commit in the state database and block store.
 func (r *Reactor) Sync(stateProvider StateProvider, discoveryTime time.Duration) (sm.State, *types.Commit, error) {
+	var (
+		state  sm.State
+		commit *types.Commit
+		err    error
+	)
+
 	r.mtx.Lock()
 	if r.syncer != nil {
 		r.mtx.Unlock()
@@ -271,19 +280,35 @@ func (r *Reactor) Sync(stateProvider StateProvider, discoveryTime time.Duration)
 	r.syncer = newSyncer(r.cfg, r.Logger, r.conn, r.connQuery, stateProvider, r.tempDir)
 	r.mtx.Unlock()
 
-	hook := func() {
-		r.Logger.Debug("Requesting snapshots from known peers")
-		// Request snapshots from all currently connected peers
+	if snapshotDir := os.Getenv("SNAPSHOT_IMPORT_DIR"); snapshotDir != "" {
+		// Do not request snapshots or chunks from the network since we're restoring from a static snapshot
+		requestSnapshots = false
 
 		r.Switch.Broadcast(p2p.Envelope{
 			ChannelID: SnapshotChannel,
 			Message:   &ssproto.SnapshotsRequest{},
 		})
+		state, commit, err = r.syncer.SyncFromLocalSnapshot(snapshotDir)
+		if err != nil {
+			// Blow up here so it's easier to spot any issues with snapshot restore
+			panic(err)
+		}
+	} else {
+
+		hook := func() {
+			r.Logger.Debug("Requesting snapshots from known peers")
+			// Request snapshots from all currently connected peers
+
+			r.Switch.Broadcast(p2p.Envelope{
+				ChannelID: SnapshotChannel,
+				Message:   &ssproto.SnapshotsRequest{},
+			})
+		}
+
+		hook()
+
+		state, commit, err = r.syncer.SyncAny(discoveryTime, hook)
 	}
-
-	hook()
-
-	state, commit, err := r.syncer.SyncAny(discoveryTime, hook)
 
 	r.mtx.Lock()
 	r.syncer = nil